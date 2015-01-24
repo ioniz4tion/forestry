24ec0e0314aea72b9e374feb89709fc451896820
Parker <iqover9000@gmail.com>
2015-01-23 16:13:30 -0800

    started seo

--- header.php
+++ header.php
@@ -20,6 +20,8 @@ Rocky Mountain Forestry</title>
 
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+		<link rel="icon" type="image/png" href="images/favicon.gif">
 		
 		<style>
 			#slides {
@@ -86,6 +88,17 @@ Rocky Mountain Forestry</title>
 
 			});
 			
+		</script>
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-58903130-1', 'auto');
+		  ga('send', 'pageview');
+
 		</script>
 
 	</head>
